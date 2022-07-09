3ebdf3a6415cf364c1a0c03fc14fc4d82e26a624
Angelo <cupertinoangelo13@gmail.com>
2022-07-09 15:59:14 -0300

    :recycle: refactor: Trocando if por case

--- sosoheart.js
+++ sosoheart.js
@@ -1,9 +1,5 @@
 require('dotenv/config');
 
-console.log("Tudo funcionando!");
-
-//bot desenvolvido por @gutoso no GitHub!
-
 const tmi = require('tmi.js');
 const botName = process.env.BOT_NAME
 const channel = process.env.CHANNEL
@@ -13,8 +9,8 @@ const token = process.env.TOKEN
 
 const opts = {
     identity: {
-    username: botName,
-    password: token
+      username: botName,
+      password: token
     },
     channels: [ channel ]
   };
@@ -23,68 +19,47 @@ const client = new tmi.client(opts);
 
 function mensagemChegou(alvo, contexto, mensagem, ehBot) {
     if (ehBot) {
-      return; //se for mensagens do bot ele não faz nada!
+      return;
     }
 
-const nomeDoComando = mensagem.trim(); // remove o espaço em branco da mensagem para verificar o comando - fica sem os pontos!
-  // checando os comandos
-  if (nomeDoComando === '!donate') {
-    console.log(`* Foi Executado o comando ${nomeDoComando}`);
-    client.say(alvo, `/me https://streamlabs.com/holly1v4/tip`);
-  } else if (nomeDoComando === '!discord') {
-    console.log(`* Foi Executado o comando ${nomeDoComando}`);
-    client.say(alvo, `/me https://discord.gg/senzHne`);
-  } else if (nomeDoComando === 'salve') {
-    console.log(`* Foi Executado o comando ${nomeDoComando}`);
-    client.say(alvo, `/me salvado meu mano!`);
-  } else if (nomeDoComando === '!youtube') {
-    console.log(`* Foi Executado o comando ${nomeDoComando}`);
-    client.say(alvo, `/me https://youtube.com/hxlly`);
-  } else if (nomeDoComando === '!uptime') {
-    console.log(`* Foi Executado o comando ${nomeDoComando}`);
-    client.say(alvo, `/me Stream uptime: $(twitch $(channel) "{{uptimeLength}}")`);
-  } else if (nomeDoComando === '!teuz') {
-    console.log(`* Foi Executado o comando ${nomeDoComando}`);
-    client.say(alvo, `/me https://www.twitch.tv/teuzz`);
-  } else if (nomeDoComando === '!teclado') {
-    console.log(`* Foi Executado o comando ${nomeDoComando}`);
-    client.say(alvo, `/me ck62`);
-  } else if (nomeDoComando === '!sens') {
-    console.log(`* Foi Executado o comando ${nomeDoComando}`);
-    client.say(alvo, `/me 800dpi - R6: 4 - CS: 1 - Fort: 10% X and Y + 50% ADS e SCOPE`);
-  } else if (nomeDoComando === '!res') {
-    console.log(`* Foi Executado o comando ${nomeDoComando}`);
-    client.say(alvo, `/me 1920x1080 - CS: 1280x960`);
-  } else if (nomeDoComando === '!react') {
-    console.log(`* Foi Executado o comando ${nomeDoComando}`);
-    client.say(alvo, `/me só mandar no chat que eu vou reagir!`);
-  } else if (nomeDoComando === '!namorada') {
-    console.log(`* Foi Executado o comando ${nomeDoComando}`);
-    client.say(alvo, `/me é o teus, sofia é esquema (mas ta com o nome no bot ne, rs)`);
-  } else if (nomeDoComando === '!mousepad') {
-    console.log(`* Foi Executado o comando ${nomeDoComando}`);
-    client.say(alvo, `/me fallen mars speed++ grande`);
-  } else if (nomeDoComando === '!mouse') {
-    console.log(`* Foi Executado o comando ${nomeDoComando}`);
-    client.say(alvo, `/me fallen 65 mars`);
-  } else if (nomeDoComando === '!headset') {
-    console.log(`* Foi Executado o comando ${nomeDoComando}`);
-    client.say(alvo, `/me hyperx cloud core 7.1`);
-  } else if (nomeDoComando === '!earnings') {
-    console.log(`* Foi Executado o comando ${nomeDoComando}`);
-    client.say(alvo, `/me dois pastel e uma coca`);
-  } else if (nomeDoComando === '!dpi') {
-    console.log(`* Foi Executado o comando ${nomeDoComando}`);
-    client.say(alvo, `/me 800`);
-  } else if (nomeDoComando === '!github') {
-    console.log(`* Foi Executado o comando ${nomeDoComando}`);
-    client.say(alvo, `/me github.com/gutoso`);
-  } else if (nomeDoComando === '!comandos') {
-    console.log(`* Foi Executado o comando ${nomeDoComando}`);
-    client.say(alvo, `/me !donate, !discord, !youtube, !uptime, !teuz, !teclado, !teclado, !sens, !res, !react, !namorada, !mousepad, !mouse, !headset, !earnings, !dpi, !github - bot desenvolvido pelo dono desta stream (@gutoso on github)`);
-  } else {
-    console.log(`* Não conheço o comando ${nomeDoComando}`);
-  }
+  const nomeDoComando = mensagem.trim();
+
+  var resposta = "";
+  switch (nomeDoComando) {
+    case "!donate":
+        resposta = 'https://streamlabs.com/marquesano/tip';
+        break;
+    case "!discord":
+        resposta = 'https://discord.gg/wyZK64NbZg';
+        break;
+    case "!youtube":
+        resposta = 'https://www.youtube.com/channel/UCZqqUeECDSfZElYwavmc0aw';
+        break;
+    case "!teclado":
+        resposta = 'Logitech G213';
+        break;
+    case "!mouse":
+        resposta = 'Logitech G403';
+        break;
+    case "!react":
+        resposta = 'Manda o link pra nos que eu abro aqui';
+        break;
+    case "!github":
+        resposta = 'https://github.com/acmachado14';
+        break;
+    case "!comandos":
+        resposta = '!donate !discord !youtube !teclado !mouse !react !github';
+        break;
+    default:
+        resposta = '0';
+        console.log(`* Não conheço o comando ${nomeDoComando}`);
+        break;
+    }
+
+    if (resposta != '0'){
+      console.log(`* Foi Executado o comando ${nomeDoComando}`);
+      client.say(alvo, resposta);
+    }
 }
 
 function entrouNoChatDaTwitch(endereco, porta) {
